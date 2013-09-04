1dc2f25d8f43a37845e7c2f5275b4466ee9a682e
Sam Lu <sam.w.lu@gmail.com>
2013-09-03 23:20:13 -0400

    added get battery level

--- hb.js
+++ hb.js
@@ -3,8 +3,11 @@
 var serialPort = require('serialport').SerialPort;
 var sp = new serialPort('/dev/ttyUSB0', { baudrate: 9600 });
 
+var buf = '';
+
 sp.on('data', function(data) {
   console.log('serial data:' + data.toString());
+  buf = buf + data;
 });
 
 sp.on('error', function(err) {
@@ -27,3 +30,18 @@ exports.rover = function(cmd) {
   return rc;
 }
 
+function getBatteryLevel() {
+  var x = buf.indexOf('[');
+  var y = buf.indexOf(']');
+  var lvl = 'n/a';
+  if (x != -1 && y != -1 && x < y) {
+    lvl =  buf.slice(x+1, y);
+    buf = '';
+  }
+  console.log('get battery level "' + buf + '" -> ' + lvl);
+  return lvl;
+}
+
+exports.status = function() {
+  return { 'battery' : getBatteryLevel() };
+}
