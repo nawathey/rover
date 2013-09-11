d8114794fb7d2c4bef4fbcc3538cbbec0d4cf1dd
Sam Lu <sam.w.lu@gmail.com>
2013-09-10 23:39:54 -0400

    start node as well

--- bin/start.sh
+++ bin/start.sh
@@ -1,22 +1,31 @@
-# sudo mount -t tmpfs -o size=10m tmpfs /tmp/stream
+# start the rover server side
+#
+# setup RAMTMP and TMPFS_SIZE by:
+# sudo vi /etc/default/tmpfs
+#
 
-# view this with http://rpi:8080/?action=stream
 
-export LD_LIBRARY_PATH=./ 
-
-TMPDIR=/tmp/stream
-mkdir -p $TMPDIR
-
-if 	\ps -ef | grep -v grep | grep raspistill >/dev/null 2>&1
-then	echo raspitill already running
-else	nohup raspistill -w 640 -h 480 -q 5 -o $TMPDIR/image.jpg -tl 100 -t 99999999 -n &
+# run node
+if 	\ps -ef | grep -v grep | grep node >/dev/null 2>&1
+then	echo node already running
+else	cd ~/rover
+	nohup node rover.js &
 fi
 
-if	[[ "$1" = "exp" ]]
+# view this with http://rpi:8080/?action=stream
+export LD_LIBRARY_PATH=./ 
+
+if	[[ "$1" = "exp" ]] 	# experimental, does not work
 then	echo start experimental MJPG streamer without file system support
 	cd ~/app/mjpg-streamer-raspi/mjpg-streamer-experimental
 	nohup ./mjpg_streamer -i "input_raspicam.so -d 100" -o "output_http.so -w ./www" &
-else	echo start MJPG streamer using $TMPDIR
+else	TMPDIR=/tmp/stream
+	mkdir -p $TMPDIR 
+	if 	\ps -ef | grep -v grep | grep raspistill >/dev/null 2>&1
+	then	echo raspitill already running
+	else	nohup raspistill -w 640 -h 480 -q 5 -o $TMPDIR/image.jpg -tl 100 -t 99999999 -n &
+	fi
+	echo start MJPG streamer using $TMPDIR
 	cd ~/app/mjpg-streamer/mjpg-streamer
 	nohup ./mjpg_streamer -i "input_file.so -f $TMPDIR -n image.jpg" -o "output_http.so -w ./www" &
 fi
