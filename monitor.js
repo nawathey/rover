99dee9e6403f9005462bd21d78466513ba692459
Samuel Lu <sam.w.lu@gmail.com>
2013-09-22 17:35:39 -0400

    catch gpio module not found

--- monitor.js
+++ monitor.js
@@ -5,23 +5,22 @@
 "use strict";
 
 try {
-  var gpio = require("gpio");
+  var g17 = require("gpio").export(17, {
+    direction: 'out',
+    interval: 200,
+  });
+  exports.use = function (io) {
+    setInterval(function () {
+      var n = Object.keys(io.sockets.manager.connected).length;
+  //      console.log('monitor: ' + n);
+      if (n > 0) {
+        g17.set();
+        setTimeout(function () { g17.reset(); }, 20);
+      }
+    }, 10000);
+  };
 } catch (e) {
-  console.log('running without gpio module, monitoring disabled');
+  exports.use = function (io) {
+    console.log('running without gpio module, monitoring disabled');
+  };
 }
-
-var g17 = gpio.export(17, {
-  direction: 'out',
-  interval: 200,
-});
-
-exports.use = function (io) {
-  setInterval(function () {
-    var n = Object.keys(io.sockets.manager.connected).length;
-//      console.log('monitor: ' + n);
-    if (n > 0) {
-      g17.set();
-      setTimeout(function () { g17.reset(); }, 20);
-    }
-  }, 10000);
-};
