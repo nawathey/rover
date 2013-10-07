9fb48735c1cfb7940f13a3075cfc3e8fe5159b52
Sam Lu <sam.w.lu@gmail.com>
2013-10-07 00:01:29 -0400

    smoother steps

--- bin/pulse.sh
+++ bin/pulse.sh
@@ -8,9 +8,9 @@ then    sudo ~/app/PiBits/ServoBlaster/user/servod --min=0 --max=2000 --p1pins=1
 fi
 
 while   :
-do      for i in 0 $(seq 0 1000 2000) $(seq 2000 -400 0) 0
+do      for i in 0 $(seq 0 100 2000) $(seq 2000 -40 0) 0
         do      echo 0=$i >/dev/servoblaster
-                sleep .1
+                sleep .01
         done
         sleep .5
 done
