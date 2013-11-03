65f566c138dc1203d997cff8e36bb9b21cf26c9d
Sam Lu <sam.w.lu@gmail.com>
2013-11-03 15:18:45 -0500

    added date separator

--- public/javascripts/motionListing.js
+++ public/javascripts/motionListing.js
@@ -22,18 +22,46 @@ $("#files").load(dir + " #files", null, function () {
     };
 
   // we can't assume the jpg and avi files are in order, so double loop
+  // first get the video files
   applyLi(function (fName, n, d, e) {
     if (e === "avi") {
       flist[n] = fName;
     }
   });
-
-  console.log('flist=' + flist);
+  // then show the static images corresponding to the video files
+  //console.log('flist=' + flist);
+  var prevDt;
   applyLi(function (fName, n, d, e) {
     if (e === "jpg") {
+      var t = d, dt;
+      if (d.length === 14) {
+        dt = d.substring(4,6) + "/" + d.substring(6,8) + "/" + d.substring(0,4);
+        t = d.substring(8,10) + ":" + d.substring(10,12) + ":" + d.substring(12,14);
+      };
+      if (dt !== prevDt) {
+        $(p).append("<h4>" + dt + "</h4>");
+        prevDt = dt;
+      };
       $(p).append("<a href='" + dir + "/" + flist[n] + "'>" +
-        "<img class='motionSnapshot' " +
-        "src='" + dir + "/" + fName + "' alt='" + d + "' /></a>");
+        "<img class='motionSnapshot' src='" + dir + "/" + fName + "' Title='" + t + "' /></a>");
     }
   });
+
+  // add hover highlighting
+  $('.motionSnapshot').hover(
+    function() {
+      $(this).css('border', "solid 2px blue");
+    }, function() {
+      $(this).css('border', "none");
+    }
+  );
 });
+
+/*
+$(document).ready(function () {
+  $('.closeSign').on('click', function() {
+    $(this).parent().hide();
+    // or any other stuff as you want
+  });
+});
+*/
