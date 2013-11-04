3513077893dd61da5bb2c54adfdc6fe9d3d70511
Sam Lu <sam.w.lu@gmail.com>
2013-11-03 22:09:29 -0500

    add ability to delete jpg/avi

--- public/javascripts/motionListing.js
+++ public/javascripts/motionListing.js
@@ -10,58 +10,72 @@ $("#files").load(dir + " #files", null, function () {
     p = $("#pics"),
     ulElement = this.childNodes[0],
     liElements = $(ulElement).children(),
-    applyLi = function (func) {
-      $(liElements).each(function (index) {
-        var fName = $(this).text(),
-          a = fName.split(/[-\.]/);
-        //console.log("working on " + a);
-        if (a.length >= 3) {
-          func(fName, a[0], a[1], a[a.length-1]);
-        }
-      });
-    };
+    prevDt;
 
+  function applyLi(func) {
+    $(liElements).each(function (index) {
+      var fName = $(this).text(),
+        a = fName.split(/[\-\.]/);
+      //console.log("working on " + a);
+      if (a.length >= 3) {
+        func(fName, a[0], a[1], a[a.length-1]);
+      }
+    });
+  }
   // we can't assume the jpg and avi files are in order, so double loop
   // first get the video files
-  applyLi(function (fName, n, d, e) {
-    if (e === "avi") {
-      flist[n] = fName;
-    }
-  });
+  applyLi(function (fName, n, d, e) { if (e === "avi") { flist[n] = {}; flist[n].avi = fName; } });
   // then show the static images corresponding to the video files
-  //console.log('flist=' + flist);
-  var prevDt;
+  //console.log("flist=" + JSON.stringify(flist));
   applyLi(function (fName, n, d, e) {
     if (e === "jpg") {
+      if (! flist[n]) {
+        console.log("no corresponding avi file for " + fName);
+        return;
+      }
       var t = d, dt;
       if (d.length === 14) {
         dt = d.substring(4,6) + "/" + d.substring(6,8) + "/" + d.substring(0,4);
         t = d.substring(8,10) + ":" + d.substring(10,12) + ":" + d.substring(12,14);
-      };
+      }
       if (dt !== prevDt) {
         $(p).append("<h4>" + dt + "</h4>");
         prevDt = dt;
-      };
-      $(p).append("<a href='" + dir + "/" + flist[n] + "'>" +
-        "<img class='motionSnapshot' src='" + dir + "/" + fName + "' Title='" + t + "' /></a>");
+      }
+      flist[n].jpg = fName;
+      $(p).append("<a href='" + dir + "/" + flist[n].avi + "' " +
+        "xfn='" + n + "'>" +
+        "<img class='motionSnapshot' src='" + dir + "/" + fName + "'" +
+        "Title='" + t + "' /></a>");
+      $(p).append("<img class='closeSign' src='/images/closeSign.png'" + fName + "'/>");
     }
   });
 
-  // add hover highlighting
-  $('.motionSnapshot').hover(
-    function() {
-      $(this).css('border', "solid 2px blue");
-    }, function() {
-      $(this).css('border', "none");
-    }
+  $(".motionSnapshot").hover(
+    function() { $(this).toggleClass("selected"); },
+    function() { $(this).toggleClass("selected"); }
   );
-});
 
-/*
-$(document).ready(function () {
-  $('.closeSign').on('click', function() {
-    $(this).parent().hide();
-    // or any other stuff as you want
+  $(".closeSign").hover(
+    function() { $(this).attr("src", "/images/closeSignActive.png"); },
+    function() { if (!$(this).prev().hasClass("deleted")) { $(this).attr("src", "/images/closeSign.png"); } }
+  );
+  $(".closeSign").on("click", function() {
+    $(this).prev().toggleClass("deleted");
+    $("#deleteButton").css("display", $(".deleted").length > 0 ? "inline" : "none");
+  });
+
+  $("#deleteButton").on("click", function() {
+    var files = [];
+    $(".deleted").each(function (index) {
+      var n = $(this).attr("xfn");
+      files.push(flist[n].jpg, flist[n].avi);
+    });
+    $.ajax({
+      type: "POST",
+      url: "/secure/api/deleteLog",
+      data: { "files" : files },
+      success: function () { console.log('deleteLog success'); location.reload() },
+    });
   });
 });
-*/
--- router.js
+++ router.js
@@ -10,19 +10,13 @@ module.exports = function (app) {
 
   app.get('/status', function (req, res) {
     require('child_process').exec("date", function (error, stdout, stderr) {
-      res.render('status', {
-        title: 'Rover - Status',
-        user: (req.session.user === undefined) ? '' : req.session.user.name,
-        status : "The time is " + stdout
-      });
+      res.render('status', { title: 'Rover - Status', status : "the time is " + stdout });
     });
   });
 
   app.get('/secure/stillFile', function (req, res) {
-    require('child_process').exec('raspistill -n -t 500 -o public/image.jpg', function (error, stdout, stderr) {
-      res.writeHead(302, {'Location': 'image.jpg'});
-      res.end();
-    });
+    require('child_process').exec('raspistill -n -t 500 -o public/image.jpg',
+      function (error, stdout, stderr) { res.writeHead(302, {'Location': 'image.jpg'}); res.end(); });
   });
 
   function getJadeParam(req, title) {
@@ -31,16 +25,19 @@ module.exports = function (app) {
     p.user = req.session.user.name;
     return p;
   }
+  app.get('/secure/home', function (req, res) { res.render('roverHome', getJadeParam(req, 'Rover Home')); });
+  app.get('/secure/drive', function (req, res) { res.render('drive', getJadeParam(req, 'Rover Drive')); });
+  app.get('/secure/motion', function (req, res) { res.render('motion', getJadeParam(req, 'Motion Camera')); });
 
-  app.get('/secure/home', function (req, res) {
-    res.render('roverHome', getJadeParam(req, 'Rover Home'));
-  });
-
-  app.get('/secure/drive', function (req, res) {
-    res.render('drive', getJadeParam(req, 'Rover Drive'));
-  });
-
-  app.get('/secure/motion', function (req, res) {
-    res.render('motion', getJadeParam(req, 'Motion Camera'));
+  var fs = require('fs');
+  app.post('/secure/api/deleteLog', function (req, res) {
+    var files = req.param("files");
+    console.log('request by ' + req.session.user.user + ' to delete files ' + files);
+    for (var i = 0; i < files.length; i++) {
+      fs.unlink( __dirname + '/public/log/' + files[i], function (e) {
+        if (e) { console.log('failed to delete (' + e + ')'); }
+      });
+    }
+    res.end();
   });
 };
